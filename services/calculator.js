63b32ce0c9a4d48a4bac30f2c33bc09db51aa7a5
Munis <munis.mahmood@yahoo.com>
2026-06-02 19:58:56 +0500

    calculator fixed

--- services/calculator.js
+++ services/calculator.js
@@ -1,17 +1,23 @@
 function calculateSqft(widthMM, heightMM, pieces = 1, layers = 1) {
 
-    const widthFt = widthMM / 304.8;
+```
+const widthFt = widthMM / 304.8;
 
-    const heightFt = heightMM / 304.8;
+const heightFt = heightMM / 304.8;
 
-    const sqft =
-        widthFt *
-        heightFt *
-        pieces;
+// Layers intentionally NOT multiplied here
+// because laminated pricing is handled separately
 
-    return Number(sqft.toFixed(2));
+const sqft =
+    widthFt *
+    heightFt *
+    pieces;
+
+return Number(sqft.toFixed(2));
+```
+
 }
 
 module.exports = {
-    calculateSqft
+calculateSqft
 };
