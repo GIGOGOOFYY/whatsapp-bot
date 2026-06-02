a89c0e453e604bf60bbff93d64150872bc3379bd
Munis <munis.mahmood@yahoo.com>
2026-06-02 17:14:48 +0500

    modified googlesheets js

--- services/googleSheets.js
+++ services/googleSheets.js
@@ -1,64 +1,23 @@
-const { google } =
-  require('googleapis')
+const { google } = require('googleapis')
 
-const auth =
-  new google.auth.GoogleAuth({
+const auth = new google.auth.GoogleAuth({
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
+  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+})
 
-    keyFile:
-      'credentials.json',
+const sheets = google.sheets({ version: 'v4', auth })
 
-    scopes: [
-      'https://www.googleapis.com/auth/spreadsheets'
-    ]
+const SPREADSHEET_ID = '1z4DpE0fL9633pmAftsmR9OTPPhngX3oQIKtlsLED4k8'
+
+async function addInquiry(phone, message, reply) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: 'Inquiries!A:D',
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [[phone, message, reply, new Date().toLocaleString()]]
+    }
   })
-
-const sheets =
-  google.sheets({
-    version: 'v4',
-    auth
-  })
-
-const SPREADSHEET_ID =
-  '1z4DpE0fL9633pmAftsmR9OTPPhngX3oQIKtlsLED4k8'
-
-async function addInquiry(
-  phone,
-  message,
-  reply
-) {
-
-  await sheets
-    .spreadsheets
-    .values
-    .append({
-
-      spreadsheetId:
-        SPREADSHEET_ID,
-
-      range:
-        'Inquiries!A:D',
-
-      valueInputOption:
-        'USER_ENTERED',
-
-      requestBody: {
-
-        values: [[
-
-          phone,
-
-          message,
-
-          reply,
-
-          new Date()
-            .toLocaleString()
-
-        ]]
-      }
-    })
 }
 
-module.exports = {
-  addInquiry
-}
+module.exports = { addInquiry }
