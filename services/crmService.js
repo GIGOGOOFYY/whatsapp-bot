c13f0d0b4c953c31c8059112e2fcaabd44566bf9
Munis <munis.mahmood@yahoo.com>
2026-06-03 14:58:31 +0500

    modified crmservice.js

--- services/crmService.js
+++ services/crmService.js
@@ -1,25 +1,22 @@
 const Customer = require("../models/Customer");
 const Inquiry = require("../models/Inquiry");
+const { addInquiry } = require("./googleSheets");
 
 async function saveInquiry(phone, message, response) {
 
-    await Inquiry.create({
-        customerPhone: phone,
-        message,
-        response
-    });
+  await Inquiry.create({ customerPhone: phone, message, response });
 
-    let customer = await Customer.findOne({
-        phone
-    });
+  let customer = await Customer.findOne({ phone });
+  if (!customer) {
+    await Customer.create({ phone });
+  }
 
-    if (!customer) {
-        await Customer.create({
-            phone
-        });
-    }
+  // Save to Google Sheet
+  try {
+    await addInquiry(phone, message, response);
+  } catch (sheetErr) {
+    console.log('Sheets Error:', sheetErr.message);
+  }
 }
 
-module.exports = {
-    saveInquiry
-};
+module.exports = { saveInquiry };
