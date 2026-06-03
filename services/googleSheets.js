f325c4ddc6ec56d3a3c82b7e8a34a8ec1f2d62e4
Munis <munis.mahmood@yahoo.com>
2026-06-03 15:15:56 +0500

    modified: googlesheet.js

--- services/googleSheets.js
+++ services/googleSheets.js
@@ -6,7 +6,6 @@ const auth = new google.auth.GoogleAuth({
 })
 
 const sheets = google.sheets({ version: 'v4', auth })
-
 const SPREADSHEET_ID = '1z4DpE0fL9633pmAftsmR9OTPPhngX3oQIKtlsLED4k8'
 
 async function addInquiry(phone, message, reply) {
@@ -14,10 +13,28 @@ async function addInquiry(phone, message, reply) {
     spreadsheetId: SPREADSHEET_ID,
     range: 'Inquiries!A:D',
     valueInputOption: 'USER_ENTERED',
+    requestBody: { values: [[phone, message, reply, new Date().toLocaleString()]] }
+  })
+}
+
+async function addCustomerLead(lead) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: 'Customers!A:H',
+    valueInputOption: 'USER_ENTERED',
     requestBody: {
-      values: [[phone, message, reply, new Date().toLocaleString()]]
+      values: [[
+        lead.phone,
+        lead.name,
+        lead.company,
+        lead.city,
+        lead.glassType,
+        lead.size,
+        lead.quantity,
+        new Date().toLocaleString()
+      ]]
     }
   })
 }
 
-module.exports = { addInquiry }
+module.exports = { addInquiry, addCustomerLead }
