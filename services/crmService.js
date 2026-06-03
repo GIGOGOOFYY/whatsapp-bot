8fa8994503156a0d9a0108b2998a52896aa8a1ed
Munis <munis.mahmood@yahoo.com>
2026-06-03 15:17:48 +0500

    modified: crmservice.js

--- services/crmService.js
+++ services/crmService.js
@@ -1,17 +1,13 @@
 const Customer = require("../models/Customer");
 const Inquiry = require("../models/Inquiry");
-const { addInquiry } = require("./googleSheets");
+const { addInquiry, addCustomerLead } = require("./googleSheets");
 
 async function saveInquiry(phone, message, response) {
-
   await Inquiry.create({ customerPhone: phone, message, response });
 
   let customer = await Customer.findOne({ phone });
-  if (!customer) {
-    await Customer.create({ phone });
-  }
+  if (!customer) await Customer.create({ phone });
 
-  // Save to Google Sheet
   try {
     await addInquiry(phone, message, response);
   } catch (sheetErr) {
@@ -19,4 +15,27 @@ async function saveInquiry(phone, message, response) {
   }
 }
 
-module.exports = { saveInquiry };
+async function saveCustomerLead(lead) {
+  try {
+    await Customer.findOneAndUpdate(
+      { phone: lead.phone },
+      {
+        phone: lead.phone,
+        name: lead.name,
+        company: lead.company,
+        city: lead.city
+      },
+      { upsert: true }
+    )
+  } catch (e) {
+    console.log('MongoDB lead error:', e.message)
+  }
+
+  try {
+    await addCustomerLead(lead)
+  } catch (e) {
+    console.log('Sheets lead error:', e.message)
+  }
+}
+
+module.exports = { saveInquiry, saveCustomerLead };
